1a0799cc83e505b639799fb70f9defe0c04d351a
Saqlain Khan <iamsaqlain25@gmail.com>
2022-05-06 21:48:28 +0530

    converted context api to redux

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
-import { CartProvider } from './contexts/cart.context';
+import App from "./App";
 
-import './index.scss';
+import "./index.scss";
+import { store } from "./store/store";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
