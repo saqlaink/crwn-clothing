919eb0bd02de247f5a1bba6c67a2ca9fa49a9797
Saqlain Khan <iamsaqlain25@gmail.com>
2022-05-07 10:13:09 +0530

    used redux thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,21 @@
 import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { useDispatch } from "react-redux";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
-import { setCategories } from "../../store/categories/category.action";
+import { fetchCategoriesAsync } from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
+      dispatch(fetchCategoriesAsync());
     };
 
     getCategoriesMap();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
